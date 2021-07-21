663c8aaad80507f7e62d070ffacd556c3fbaf7de
NeNeTheCreative <shecodes@nenethecreative.com>
2021-07-21 11:01:10 -0400

    task 6 completed

--- index.js
+++ index.js
@@ -108,10 +108,15 @@ Use the higher order function getAverageGoals to do the following:
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
-function getAverageGoals(/* code here */) {
-   /* code here */
+function getAverageGoals(getFinalscb) {
+    const averageNum = getFinalscb.reduce(function(acc, item){
+        return acc + item['Home Team Goals'] + item['Away Team Goals'];
+    },0);
+   return (averageNum / getFinalscb.length).toFixed(2);
+   
 }
 
+console.log('task 6', getAverageGoals(getFinals(fifaData)));
 
 
 
